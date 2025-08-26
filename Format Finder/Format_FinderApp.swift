eb7097d17dce30f39e1da95debadf924d9460409
Connor Murphy <cmurphy1140@gmail.com>
2025-08-26 18:25:23 -0400

    Initial Commit

---
+++ Format Finder/Format_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Format_FinderApp.swift
+//  Format Finder
+//
+//  Created by Connor A Murphy on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Format_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
